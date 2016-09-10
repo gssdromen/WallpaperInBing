250a7e93c9ac5b5fd719673be2458a31e60a2ffc
gssdromen <gssdromen@gssdromens-Mac-mini.local>
2016-09-10 08:21:40 +0800

    优化路径书写

--- Main.py
+++ Main.py
@@ -6,6 +6,7 @@ import urllib
 import re
 import time
 import os
+import sys
 
 def getHtml(url):
     return urllib.urlopen(url).read()
@@ -23,9 +24,19 @@ def downloadImg(url,path):
     print xpath
     urllib.urlretrieve(url,xpath)
 
+    #获取脚本文件的当前路径
+def cur_file_dir():
+     #获取脚本路径
+     path = sys.path[0]
+     #判断为脚本文件还是py2exe编译后的文件，如果是脚本文件，则返回的是脚本的目录，如果是py2exe编译后的文件，则返回的是编译后的文件路径
+     if os.path.isdir(path):
+         return path
+     elif os.path.isfile(path):
+         return os.path.dirname(path)
+
 if __name__=='__main__':
     start=time.time()
     html=getHtml('http://cn.bing.com/')
     url=getImgUrl(html)
-    downloadImg(url,'/Users/dromenwu/Documents/python/wallpaperInBing/wallpapers')
+    downloadImg(url,os.path.join(cur_file_dir(),'wallpapers'))
     end=time.time()
